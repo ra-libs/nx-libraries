4a92e82a063a57262569459da00f29e6236439cd
Rabah Zeineddine <rabah.zeineddine@hotmail.com>
2023-10-01 21:09:05 -0300

    feat: add initial rbac react lib

---
+++ packages/react-rbac/src/lib/react-rbac.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface ReactRbacProps {}
+
+export function ReactRbac(props: ReactRbacProps) {
+  return (
+    <div>
+      <h1>Welcome to ReactRbac!</h1>
+    </div>
+  );
+}
+
+export default ReactRbac;
